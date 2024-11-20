80a07761b191668cc2b2923b30abb8d7fb0022ec
Ali Haider <aliprogrammer309@gmail.com>
2024-11-21 01:02:43 +0500

    blogs page setup

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,5 +1,32 @@
-const page = () => {
-  return <div>blog page</div>;
+import styles from "./blog.module.css";
+import PostCard from "../../components/postCard/postCard";
+
+const post = {
+  img: '/about.png',
+  createdAt: Date.now(),
+  title: 'Post Title',
+  body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque, velit?',
+  slug: '/',
+}
+
+const BlogPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.post}>
+        <PostCard post={post} />
+      </div>
+      <div className={styles.post}>
+        <PostCard post={post} />
+      </div>
+      <div className={styles.post}>
+        <PostCard post={post} />
+      </div>
+      <div className={styles.post}>
+        <PostCard post={post} />
+      </div>
+
+    </div>
+  );
 };
 
-export default page;
+export default BlogPage;
