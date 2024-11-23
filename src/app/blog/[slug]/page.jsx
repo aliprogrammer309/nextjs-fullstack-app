369ce926e77dd881ee6fa6c9f07ffe974d1ea8a7
Ali Haider <aliprogrammer309@gmail.com>
2024-11-23 23:49:31 +0500

    single post component updates

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,5 +1,26 @@
-const page = () => {
-  return <div>single blog page</div>;
+import Image from "next/image";
+import styles from "./singlePost.module.css";
+import PostUser from "../../../components/postUser/postUser";
+
+const SinglePostPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/about.png" alt="" fill className={styles.img} />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Post Title</h1>
+        <div className={styles.detail}>
+          <PostUser />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>{Date.now()}</span>
+          </div>
+        </div>
+        <div className={styles.content}>Post Description...</div>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default SinglePostPage;
