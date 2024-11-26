3b799c757496097bb318884f892700bcd7888c86
Ali Haider <aliprogrammer309@gmail.com>
2024-11-26 23:52:20 +0500

    hooked up API to get single post data

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -2,14 +2,27 @@ import Image from "next/image";
 import styles from "./singlePost.module.css";
 import PostUser from "../../../components/postUser/postUser";
 
-const SinglePostPage = () => {
+const fetchData = async (slug) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
+
+  if (!res.ok) {
+    throw new Error("Error getching the posts!");
+  }
+
+  return res.json();
+};
+
+const SinglePostPage = async ({ params }) => {
+  const { slug } = params;
+  const post = await fetchData(slug);
+
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Image src="/about.png" alt="" fill className={styles.img} />
       </div>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>Post Title</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
           <PostUser />
           <div className={styles.detailText}>
@@ -17,7 +30,7 @@ const SinglePostPage = () => {
             <span className={styles.detailValue}>{Date.now()}</span>
           </div>
         </div>
-        <div className={styles.content}>Post Description...</div>
+        <div className={styles.content}>{post.body}</div>
       </div>
     </div>
   );
