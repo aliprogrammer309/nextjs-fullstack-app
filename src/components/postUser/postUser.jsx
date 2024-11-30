34fe8f86b1ac50c25cc27b170e797c2be8fa10b6
Ali Haider <aliprogrammer309@gmail.com>
2024-11-30 21:50:56 +0500

    fixed the post user

--- src/components/postUser/postUser.jsx
+++ src/components/postUser/postUser.jsx
@@ -26,7 +26,7 @@ const PostUser = async ({ userId }) => {
       />
       <div className={styles.texts}>
         <span className={styles.title}>Author</span>
-        <span className={styles.username}>{user.username}</span>
+        <span className={styles.username}>{user.email}</span>
       </div>
     </div>
   );
